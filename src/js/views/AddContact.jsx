d9aa5d31947262637d0d66f9dd7267bf895f8ad7
Ramiro Correa <98496459+ramicorrea21@users.noreply.github.com>
2023-10-05 19:37:21 +0000

    delete contact

--- src/js/views/AddContact.jsx
+++ src/js/views/AddContact.jsx
@@ -1,33 +1,31 @@
 import React , {useState, useContext} from "react";
 import { Context } from "../store/appContext";
-import { json } from "react-router";
+import { Link } from "react-router-dom";
 const AddContact = () => {
     const {store, actions} = useContext(Context)
-
-
 
     const [newContact, setNewContact] = useState()
     const [fullname, setFullname] = useState('')
     const [address, setAddress] = useState('')
     const [phone, setPhone] = useState()
     const [email, setEmail] = useState('')
-
+ 
     const addNewContact = async () =>{
-        setNewContact({
-            "address" : address,
-            "agenda_slug": "ramicorrea21",
-            "email" : email,
-            "full_name" : fullname,
-            "phone" : phone,
+         setNewContact({
+            address : address,
+            agenda_slug: "ramicorrea21",
+            email : email,
+            full_name : fullname,
+            phone : phone,
         })
-        
+        if (newContact != undefined){
             try {
-                let response = await fetch("https://playground.4geeks.com/apis/fake/contact/", {
-                    method : "POST",
-                    headers:{
+                let response = await fetch('https://playground.4geeks.com/apis/fake/contact/',{
+                    method: "POST",
+                    headers :{
                         "Content-Type": "application/json"
                     },
-                    body : JSON.stringify(newContact)
+                    body: JSON.stringify(newContact)
                 })
                 if(response.ok){
                     actions.getAllContacts()
@@ -35,12 +33,19 @@ const AddContact = () => {
             } catch (error) {
                 console.log(error);
             }
+        }
+      
     }
+    const handleSubmit = (e) =>{
+        e.preventDefault() 
+        addNewContact()
+    }
+
 
     return (
         <div className="container">
             <h1 className="text-center">Add a contact</h1>
-            <form onSubmit={(e) => { e.preventDefault() }}>
+            <form onSubmit={handleSubmit}>
                 <label>Full Name</label>
                 <input className="form-control form-control-lg" type="text" placeholder="full name" onChange={(e) => setFullname(e.target.value)}/>
                 <label>Email</label>
@@ -50,9 +55,12 @@ const AddContact = () => {
                 <label>Address</label>
                 <input className="form-control form-control-lg" type="text" placeholder="Address" onChange={(e) => setAddress(e.target.value)} />
                 <div className="d-grid gap-2 mt-2">
-                    <button type="submit" className="btn btn-primary" onClick={addNewContact}>Save</button>
+                    <button  className="btn btn-primary">Save</button>
                 </div>
             </form>
+                <Link to="/"><button className="btn btn-outline-success my-2">Go back to contacts</button></Link>
+                
+
         </div>
     )
 }
